c9d8dfef0be57f2a05d05964e451bde8f46320e9
Joona Piirainen <joona.piirainen@gmail.com>
2021-04-17 19:18:40 +0300

    utils

---
+++ src/util/decode.ts
@@ -0,0 +1,14 @@
+import * as t from 'io-ts';
+import { E, pipe } from './fpts';
+
+export type DecodeError = { tag: 'decodeError'; errors: t.Errors };
+
+export const decodeError = (errors: t.Errors): DecodeError => ({
+   tag: 'decodeError',
+   errors,
+});
+
+export const decodeWithCodec = <A>(codec: t.Type<A>) => (
+   value: unknown
+): E.Either<DecodeError, A> =>
+   pipe(codec.decode(value), E.mapLeft(decodeError));
---
+++ src/util/typeGuards.ts
@@ -0,0 +1,22 @@
+export type Nullable<A> = A | null;
+
+export type Undefineable<A> = A | undefined;
+
+export type Nil = null | undefined;
+
+export type Nilable<A> = A | Nil;
+
+export const isNull = (x: unknown): x is null => x === null;
+
+export const isUndefined = (x: unknown): x is undefined => x === undefined;
+
+export const isNil = (x: unknown): x is Nil => isNull(x) || isUndefined(x);
+
+export const isNotUndefined = <A>(
+   x: Undefineable<A>
+): x is Exclude<A, undefined> => !isUndefined(x);
+
+export const isNotNull = <A>(x: Nullable<A>): x is Exclude<A, null> =>
+   !isNull(x);
+
+export const isNotNil = <A>(x: Nilable<A>): x is Exclude<A, Nil> => !isNil(x);
