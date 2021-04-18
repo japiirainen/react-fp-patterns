0fdbd9652627b42ca0d85552e36b9e41e63142eb
Joona Piirainen <joona.piirainen@gmail.com>
2021-04-18 18:40:06 +0300

    localStorage module

---
+++ src/service/localStorage/DomLocalStorage.ts
@@ -0,0 +1,15 @@
+import { O } from '../../util/fpts';
+import { LocalStorage } from './LocalStorage';
+
+export const domLocalStorage: LocalStorage = {
+   getItem: (key: string) => () => O.fromNullable(localStorage.getItem(key)),
+
+   setItem: (key: string, value: string) => () =>
+      localStorage.setItem(key, value),
+
+   removeItem: (key: string) => () => localStorage.removeItem(key),
+
+   clear: () => localStorage.clear(),
+
+   size: () => localStorage.length,
+};
--- src/service/localStorage/LocalStorage.ts
+++ src/service/localStorage/LocalStorage.ts
@@ -1,7 +1,6 @@
-import { publicEncrypt } from 'crypto';
 import * as t from 'io-ts';
 import { DecodeError, decodeWithCodec } from '../../util/decode';
-import { E, IO, O, pipe, R, RTE } from '../../util/fpts';
+import { E, IO, O, pipe, RTE } from '../../util/fpts';
 
 /////////////////////////////////////////////////////////
 // Intarfaces
@@ -38,4 +37,79 @@ export const setItem = (
    pipe(
       RTE.ask<LocalStorageEnv>(),
       RTE.chain(env => RTE.fromIO(env.localStorage.setItem(key, value)))
+   );
+
+export const removeItem = (
+   key: string
+): RTE.ReaderTaskEither<LocalStorageEnv, never, void> =>
+   pipe(
+      RTE.ask<LocalStorageEnv>(),
+      RTE.chain(env => RTE.fromIO(env.localStorage.removeItem(key)))
+   );
+
+export const clear: RTE.ReaderTaskEither<LocalStorageEnv, never, void> = pipe(
+   RTE.ask<LocalStorageEnv>(),
+   RTE.chain(env => RTE.fromIO(env.localStorage.clear))
+);
+
+export const size: RTE.ReaderTaskEither<LocalStorageEnv, never, number> = pipe(
+   RTE.ask<LocalStorageEnv>(),
+   RTE.chain(env => RTE.fromIO(env.localStorage.size))
+);
+
+export const getItemWithDecode = <E, A>(
+   key: string,
+   decode: (raw: unknown) => E.Either<E, A>
+): RTE.ReaderTaskEither<LocalStorageEnv, E, O.Option<A>> =>
+   pipe(
+      getItem(key),
+      RTE.chainEitherKW(itemStringOpt =>
+         pipe(
+            itemStringOpt,
+            O.fold(
+               (): E.Either<E, O.Option<A>> => E.right(O.none),
+               itemString => pipe(itemString, JSON.parse, decode, E.map(O.some))
+            )
+         )
+      )
+   );
+
+export const setItemWithEncode = <A>(
+   key: string,
+   item: A,
+   encode: (a: A) => unknown
+): RTE.ReaderTaskEither<LocalStorageEnv, never, void> =>
+   pipe(item, encode, JSON.stringify, val => setItem(key, val));
+
+export const getItemWithCache = <RGet, EGet, A>(
+   key: string,
+   codec: t.Type<A>,
+   get: RTE.ReaderTaskEither<RGet, EGet, A>
+): RTE.ReaderTaskEither<LocalStorageEnv & RGet, EGet | DecodeError, A> =>
+   pipe(
+      // Try to get from the localStorage cache
+      getItemWithDecode(key, decodeWithCodec(codec)),
+      RTE.chainW(dataOpt =>
+         pipe(
+            dataOpt,
+            O.fold(
+               // Cache miss - do the API call, and store the result in the cache
+               () =>
+                  pipe(
+                     // Do get call
+                     get,
+                     RTE.chainW(data =>
+                        pipe(
+                           // store the result as a side-effect
+                           setItemWithEncode(key, data, codec.encode),
+                           // return the result
+                           RTE.map(_ => data)
+                        )
+                     )
+                  ),
+               // Cache hit - just retun it
+               RTE.right
+            )
+         )
+      )
    );
